b25d3798622d1e4541e4fdc2c9660a6f2da4211d
Rayane-T <slamaper1@gmail.com>
2022-06-15 02:10:31 +0200

    resume maj

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 import Navbar from "../components/Navbar"
+import resumepdf from "../image/resume.pdf"
 
 export default function Resume(){
     return(
-        <Navbar/>
-        
+        <>
+            <Navbar/>
+            <embed src={resumepdf} type="application/pdf" width="100%" height="100%"></embed>
+        </>
     )
 }
