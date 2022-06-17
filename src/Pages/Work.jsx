2bb2d4b0a529721901fc2ddd77de7e8980a858d9
Rayane-T <slamaper1@gmail.com>
2022-06-17 23:55:11 +0200

    .

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -7,7 +7,7 @@ export default function Work(){
         <div>
             <Navbar/>
             <div className="hero--work">
-                <h2>Internship Experience 👔</h2>
+                <h2>Internship Experience !</h2>
                 <h3>Here you can see all the places I've worked at so far, as well as what I did there.</h3>
                 <h4>- Each section outlines the company, the role, my tenure and a brief description of what my job entailed</h4>
             </div>
