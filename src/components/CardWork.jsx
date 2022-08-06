09b0a102b52917b1389dc727773843e78a0ce43d
Rayane Tayache <slamaper1@gmail.com>
2022-08-07 00:06:46 +0200

    cardWork uptated

--- src/components/CardWork.jsx
+++ src/components/CardWork.jsx
@@ -1,17 +1,162 @@
 import React from "react"
+import {FaGithubSquare,FaLinkedin,FaInstagramSquare} from "react-icons/fa"
 import logo from "../image/FedEx_logo.png"
 
 export default function CardWork() {
     return (
-        <section className="card--work">
-            <div className="card--info">
-                <div>FedEx</div>
-                <div>Sorting agent manager</div>
-                <div>June 2021- August 2021</div>
-                <div>- Incoming Software Engineer Intern at Citadel for Global Quantitative Strategies.</div>
+        <section>
+            <main>
+              <h1>
+                If you want to get lost in social media, we got you. Check these out.
+              </h1>
+        
+              <form class="form">
+                <i class="fas fa-search"
+                  ><input type="search" placeholder="Find top social media websites"
+                /></i>
+              </form>
+            </main>
+            <div class="container">
+              <h3>Top social media websites of the year</h3>
+        
+              <div class="cards">
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-facebook"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Facebook</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-twitter"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Twitter</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-whatsapp"></i>
+                  </div>
+        
+                  <div>
+                    <h4>WhatsApp</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-instagram"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Instagram</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-youtube"></i>
+                  </div>
+        
+                  <div>
+                    <h4>YouTube</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-linkedin"></i>
+                  </div>
+        
+                  <div>
+                    <h4>LinkedIn</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-snapchat"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Snapchat</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-telegram"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Telegram</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+        
+                <div class="cards-inner">
+                  <div>
+                    <i class="fab fa-tiktok"></i>
+                  </div>
+        
+                  <div>
+                    <h4>Tiktok</h4>
+                    <small>Social Media</small>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Praesentium, temporibus.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <img classname="comp-logo" src={logo} alt="logo fedex" />
-        </section>
+            </section>
     )
 }
 
