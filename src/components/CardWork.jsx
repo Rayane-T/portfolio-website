af3af29626e12d7d39b049ccb36afec622274301
Rayane Tayache <slamaper1@gmail.com>
2022-08-07 01:09:14 +0200

    cardwork uptated

--- src/components/CardWork.jsx
+++ src/components/CardWork.jsx
@@ -1,162 +1,27 @@
 import React from "react"
-import {FaGithubSquare,FaLinkedin,FaInstagramSquare} from "react-icons/fa"
-import logo from "../image/FedEx_logo.png"
+import logo from '../image/R-logo-removebg-preview.png'
 
 export default function CardWork() {
     return (
         <section>
-            <main>
-              <h1>
-                If you want to get lost in social media, we got you. Check these out.
-              </h1>
-        
-              <form class="form">
-                <i class="fas fa-search"
-                  ><input type="search" placeholder="Find top social media websites"
-                /></i>
-              </form>
-            </main>
             <div class="container">
-              <h3>Top social media websites of the year</h3>
-        
               <div class="cards">
                 <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-facebook"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Facebook</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
+                    <div>
+                        <h4>Freelancer</h4>
+                        <small>Independant</small>
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Praesentium, temporibus.
+                        </p>
+                    </div>
+                    <div>
+                        <img src={logo} height="100px"/>
+                    </div>
                 </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-twitter"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Twitter</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-whatsapp"></i>
-                  </div>
-        
-                  <div>
-                    <h4>WhatsApp</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-instagram"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Instagram</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-youtube"></i>
-                  </div>
-        
-                  <div>
-                    <h4>YouTube</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-linkedin"></i>
-                  </div>
-        
-                  <div>
-                    <h4>LinkedIn</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-snapchat"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Snapchat</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-telegram"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Telegram</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-        
-                <div class="cards-inner">
-                  <div>
-                    <i class="fab fa-tiktok"></i>
-                  </div>
-        
-                  <div>
-                    <h4>Tiktok</h4>
-                    <small>Social Media</small>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Praesentium, temporibus.
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
-            </section>
+        </div>
+    </section>
     )
 }
 
