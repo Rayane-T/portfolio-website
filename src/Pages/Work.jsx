fe9c9b6a2df99351ef7f89038753677caae1215f
Rayane Tayache <slamaper1@gmail.com>
2022-08-10 00:57:35 +0200

    container--button uptaded

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -10,14 +10,12 @@ export default function Work(){
                 <h2>Internship Experience !</h2>
                 <h3>Here you can see all the places I've worked at so far, as well as what I did there.</h3>
                 <h4>- Each section outlines the company, the role, my tenure and a brief description of what my job entailed</h4>
+                <div className="container--button">
+                    <button className="ui--button-first">LinkedIn</button>
+                    <button className="ui--button-last">Resume</button>
+                </div>
             </div>
             <CardWork/>
         </div>
     )
 }
-/*
-                <div>
-                    <button>LinkedIn</button>
-                    <button>Resume</button>
-                </div> 
-*/
